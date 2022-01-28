ff512c52970f82a730a569821ca2e2bc6a56597d
nghiemmanhcuong <nghiemmanhcuong98@gmail.com>
2022-01-28 16:39:25 +0700

    update source code

--- src/index.js
+++ src/index.js
@@ -5,40 +5,40 @@ import Layout from './components/Layout';
 import './scss/index.scss';
 import {store} from './store/store';
 import {Provider} from 'react-redux';
-import product_01_image_01 from '../images/products/product-01 (1).jpg';
-import product_01_image_02 from '../images/products/product-01 (2).jpg';
-import product_02_image_01 from '../images/products/product-02 (1).jpg';
-import product_02_image_02 from '../images/products/product-02 (2).jpg';
+import product_01_image_01 from './assets/images/products/product-01 (1).jpg';
+import product_01_image_02 from './assets/images/products/product-01 (2).jpg';
+import product_02_image_01 from './assets/images/products/product-02 (1).jpg';
+import product_02_image_02 from './assets/images/products/product-02 (2).jpg';
 
-import product_03_image_01 from '../images/products/product-03 (1).jpg';
-import product_03_image_02 from '../images/products/product-03 (2).jpg';
+import product_03_image_01 from './assets/images/products/product-03 (1).jpg';
+import product_03_image_02 from './assets/images/products/product-03 (2).jpg';
 
-import product_04_image_01 from '../images/products/product-04 (1).jpg';
-import product_04_image_02 from '../images/products/product-04 (2).jpg';
+import product_04_image_01 from './assets/images/products/product-04 (1).jpg';
+import product_04_image_02 from './assets/images/products/product-04 (2).jpg';
 
-import product_05_image_01 from '../images/products/product-05 (1).jpg';
-import product_05_image_02 from '../images/products/product-05 (2).jpg';
+import product_05_image_01 from './assets/images/products/product-05 (1).jpg';
+import product_05_image_02 from './assets/images/products/product-05 (2).jpg';
 
-import product_06_image_01 from '../images/products/product-06 (1).jpg';
-import product_06_image_02 from '../images/products/product-06 (2).jpg';
+import product_06_image_01 from './assets/images/products/product-06 (1).jpg';
+import product_06_image_02 from './assets/images/products/product-06 (2).jpg';
 
-import product_07_image_01 from '../images/products/product-07 (1).jpg';
-import product_07_image_02 from '../images/products/product-07 (2).jpg';
+import product_07_image_01 from './assets/images/products/product-07 (1).jpg';
+import product_07_image_02 from './assets/images/products/product-07 (2).jpg';
 
-import product_08_image_01 from '../images/products/product-08 (1).jpg';
-import product_08_image_02 from '../images/products/product-08 (2).jpg';
+import product_08_image_01 from './assets/images/products/product-08 (1).jpg';
+import product_08_image_02 from './assets/images/products/product-08 (2).jpg';
 
-import product_09_image_01 from '../images/products/product-09 (1).jpg';
-import product_09_image_02 from '../images/products/product-09 (2).jpg';
+import product_09_image_01 from './assets/images/products/product-09 (1).jpg';
+import product_09_image_02 from './assets/images/products/product-09 (2).jpg';
 
-import product_10_image_01 from '../images/products/product-10 (1).jpg';
-import product_10_image_02 from '../images/products/product-10 (2).jpg';
+import product_10_image_01 from './assets/images/products/product-10 (1).jpg';
+import product_10_image_02 from './assets/images/products/product-10 (2).jpg';
 
-import product_11_image_01 from '../images/products/product-11 (1).jpg';
-import product_11_image_02 from '../images/products/product-11 (2).jpg';
+import product_11_image_01 from './assets/images/products/product-11 (1).jpg';
+import product_11_image_02 from './assets/images/products/product-11 (2).jpg';
 
-import product_12_image_01 from '../images/products/product-12 (1).jpg';
-import product_12_image_02 from '../images/products/product-12 (2).jpg';
+import product_12_image_01 from './assets/images/products/product-12 (1).jpg';
+import product_12_image_02 from './assets/images/products/product-12 (2).jpg';
 
 ReactDOM.render(
     <React.StrictMode>
